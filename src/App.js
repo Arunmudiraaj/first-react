ad113aec2c293296f3cc781682d27959df57497e
arun mudiraj <arunmudiraj18@gmail.com>
2023-01-28 11:10:06 +0530

    first react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MyItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <MyItem></MyItem>
+      
+    </div>
+  );
+}
+
+export default App;
