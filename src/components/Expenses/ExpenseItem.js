289dd210ded3678dfbb43d6e2d131ad100ac4ced
arun mudiraj <arunmudiraj18@gmail.com>
2023-01-29 12:12:44 +0530

    change title and delete button on items

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,21 @@ import Card from "../UI/Card";
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
 const ExpenseItem =(props)=> {
-    
+    const clickHandler = ()=>{
+      console.log("clicked")
+    }
+    const deleteHandler = (e)=>{
+      console.log(e.target.parentNode)
+      const r = document.querySelector("#root").firstChild
+      r.removeChild(e.target.parentNode)
+    }
     
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date}/>
       <ExpenseDetails title = {props.title} amount = {props.amount}/>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete</button>
     </Card>
   );
 }
