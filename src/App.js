a001f72d14887f6b360508afcce0ab6d9cc1496b
arun mudiraj <arunmudiraj18@gmail.com>
2023-01-30 10:52:23 +0530

    adding form and adding onChange listner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import MyItem from "./components/Expenses/ExpenseItem";
-
+// import ExpenseDate from './components/Expenses/ExpenseForm'
+import ExpenseForm from "./components/Expenses/ExpenseForm";
 const App = () => {
   const expenses = [
     {
@@ -30,6 +31,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseForm/>
       <MyItem
         title={expenses[0].title}
         amount={expenses[0].amount}
